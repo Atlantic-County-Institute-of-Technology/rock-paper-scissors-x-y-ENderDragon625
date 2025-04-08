68c951bf37d2ac5ddbad0004bd122e3fe7e87316
newm0294 <newm0294@student.acitech.org>
2025-04-08 08:15:56 -0400

    Project 2.0 Done: Everything ig

--- script.js
+++ script.js
@@ -1,157 +1,149 @@
 
 
-const playerScoreBoard = document.querySelector('.p-count');
-const computerScoreBoard = document.querySelector('.c-count');
-const result = document.querySelector('.result');
-let victory = new Audio("6am.mp3")
-let defeat = new Audio("Sad Violin.mp3")
+const player_score = document.getElementById('player-score');
+const computer_score = document.getElementById('computer-score');
+const player_choice = document.getElementById('player-choice');
+const computer_choice = document.getElementById('computer-choice');
+const result = document.getElementById('result');
+const reset_button = document.getElementById('reset-btn');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
+const dwight = document.getElementById('dwight');
+const yogaball = document.getElementById('yogaball');
+const choices = document.querySelectorAll('.choice');
 
-const game = () => {
-    let playerScore = 0;
-    let computerScore = 0;
-    let moves = 0;
-
-    const playGame = () => {
-        const rockBtn = document.querySelector('.rock');
-        const paperBtn = document.querySelector('.paper');
-        const scissorBtn = document.querySelector('.scissor');
-        const dwightBtn = document.querySelector('.dwight');
-        const yogaballBtn = document.querySelector('.yogaball');
-        const playerOptions = [rockBtn, paperBtn, scissorBtn, dwightBtn, yogaballBtn];
-        const computerOptions = ['rock', 'paper', 'scissors', 'dwight', 'yogaball']
-
-        // Function to start playing game
-        playerOptions.forEach(option => {
-            option.addEventListener('click', function () {
-
-                const movesLeft = document.querySelector('.movesleft');
-                moves++;
-                movesLeft.innerText = `Moves Left: ${10 - moves}`;
-
-                const choiceNumber = Math.floor(Math.random() * 5);
-                const computerChoice = computerOptions[choiceNumber];
-
-                // Function to check who wins
-                winner(this.innerText, computerChoice)
-
-                // Calling gameOver function after 10 moves
-                if (moves == 10) {
-                    gameOver(playerOptions, movesLeft);
-                }
-            })
-        })
-
-    }
-
-    function winner(player, computer) {
-
-        player = player.toLowerCase();
-        computer = computer.toLowerCase();
-        if (player === computer) {
-            result.textContent = 'Tie'
-        }
-        else if (player.localeCompare('rock')) {
-            if (computer == 'paper' || computer == 'yogaball') {
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-
-            } else {
-                result.textContent = 'Player Won'
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if (player.localeCompare('scissors')) {
-            if (computer == 'rock') {
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            } else {
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if (player.localeCompare('paper')) {
-            if (computer == 'scissors' || computer == 'dwight' || computer == 'yogaball' ) {
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            } else {
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if (player.localeCompare('dwight')) {
-            if (computer == 'scissors'  || computer == 'rock') {
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            } else {
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-        else if (player.localeCompare('yogaball')) {
-            if (computer == 'dwight' || computer == 'scissors') {
-                result.textContent = 'Computer Won';
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-            } else {
-                result.textContent = 'Player Won';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-        }
-    }
-
-    const gameOver = (playerOptions, movesLeft) => {
-
-        const chooseMove = document.querySelector('.move');
-        const result = document.querySelector('.result');
-        const reloadBtn = document.querySelector('.reload');
-
-        playerOptions.forEach(option => {
-            option.style.display = 'none';
-        })
+// scorekeeping values
+let player_score_val = 0;
+let computer_score_val = 0;
 
 
-        chooseMove.innerText = 'Game Over!!'
-        movesLeft.style.display = 'none';
 
-        if (playerScore > computerScore) {
-            result.style.fontSize = '2rem';
-            result.innerText = 'You Won The Game'
-            result.style.color = '#308D46';
-            let victory = new Audio("Assets/6am.mp3");
-        victory.play();
-        
-        }
-        else if (playerScore < computerScore) {
-            result.style.fontSize = '2rem';
-            result.innerText = 'You Lost The Game';
-            result.style.color = 'red';
-            let defeat = new Audio("Assets/Sad Violin.mp3")
-            defeat.play();
-        }
-        else {
-            result.style.fontSize = '2rem';
-            result.innerText = 'Tie';
-            result.style.color = 'grey'
-        }
-        reloadBtn.innerText = 'Restart';
-        reloadBtn.style.display = 'flex'
-        reloadBtn.addEventListener('click', () => {
-            window.location.reload();
-        })
+// selection list for choosing rock, paper, or scissors
+const choices_list = ['rock', 'paper','scissors', 'dwight','yogaball'];
+// selection list for choosing the icon for player & computer
+const choice_icons = ['assets/rock.png', 'assets/transparent-image-lined-paper-school-book-26.png', 'assets/scissors-12.png', 'assets/DwightSchrute.png', 'assets/pngimg.com - exercise_ball_PNG111131.png']
 
-    }
+// actionListeners for buttons
+reset_button.addEventListener('click', () => {
+    console.log("Reset button clicked.");
+    reset_game();
+})
 
-    playGame();
 
+// for each button, add the check_winner function
+choices.forEach(choice => {
+    choice.addEventListener('click', () => {
+        console.log("Choice button clicked.")
+        // choose the index of the target button id, then have the cpu choose a random value
+        check_winner((choices_list.indexOf(choice.id)+1), cpu_choose_item());
+    })
+})
+
+// returns a random integer value from 1-3 (1 = rock, 2 = scissors, etc.)
+function cpu_choose_item() { 
+    return Math.floor(Math.random() * choices.length) + 1 
 }
 
-game();
+// compare the player's selected item to the CPU's
+function check_winner(player, cpu) {
+    console.log(`Player: ${player} | Computer: ${cpu}`);
+    let winner;
+    // if the values are equal, tie
+    if(player == cpu)
+    {
+        winner = "Tie!"
+    }
+    // if player chose rock
+    else if(player == 1) {
+        // if cpu chooses scissors, player wins
+        if(cpu == 3 || cpu == 4) {
+            winner = "Player Wins!"
+        }
+        else 
+            winner = "CPU Wins!"
+
+    }    
+    // if player chose paper
+    else if(player == 2) {
+        // if cpu chooses rock, player wins
+        if(cpu == 1 || cpu == 5) {
+            winner = "Player Wins!"
+        }
+        else 
+            winner = "CPU Wins!"
+    }
+    // if player chose scissors
+    else if(player == 3) {
+        // if cpu chooses paper, player wins
+        if(cpu == 2 || cpu == 5) {
+            winner = "Player Wins!"
+        }
+        else 
+            winner = "CPU Wins!"
+    }
+    else if(player == 4) {
+        // if cpu chooses paper, player wins
+        if(cpu == 2 || cpu == 3) {
+            winner = "Player Wins!"
+        }
+        else 
+            winner = "CPU Wins!"
+    }
+    else if(player == 5) {
+        // if cpu chooses paper, player wins
+        if(cpu == 1 || cpu == 4) {
+            winner = "Player Wins!"
+        }
+        else 
+            winner = "CPU Wins!"
+    }
+
+    if(winner.match("Player Wins!")) {
+        player_score_val++;
+    }
+    else if(winner.match("CPU Wins!")) {
+        computer_score_val++;
+    }
+
+    // update the interface with the current choice values, and the winner
+    updateUI(player, cpu, winner);
+}
+
+// update the UI with the corresponding values for player, cpu, and winner
+function updateUI(player, cpu, winner) {
+    result.textContent = winner;
+    
+    // empty out any images stored previously
+    player_choice.innerHTML = '';
+    computer_choice.innerHTML = '';
+    
+    // create a new image, set the source to the player's
+    // choice, format the size, then append to player_choice
+    let p_img = new Image();
+    p_img.src = choice_icons[player-1];
+    p_img.classList.add('choice_image'); 
+    player_choice.appendChild(p_img); 
+
+    // create a new image, set the source to the cpu's
+    // choice, format the size, then append to computer_choice
+    let c_img = new Image();
+    c_img.src = choice_icons[cpu-1];
+    c_img.classList.add('choice_image');
+    computer_choice.appendChild(c_img);
+
+    player_score.innerText = player_score_val;
+    computer_score.innerText = computer_score_val;
+}
+
+// function to reset the UI and all associated values
+function reset_game() {
+    // reset the scores to zero
+    player_score_val = 0;
+    computer_score_val = 0;
+    
+    // update the UI with nothing and a starter prompt
+    updateUI('','',"Choose your weapon!");
+    player_choice.innerHTML = '';
+    computer_choice.innerHTML = '';
+}
